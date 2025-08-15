ea1606646a47a6de70a210954f8f69b795cf70e7
Somebro <someBroYouKnow@users.noreply.github.com>
2025-08-16 01:35:07 +0530

    damn

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,41 @@
+'use client';
+import { useState } from 'react';
 import styles from  './navbar.module.css'
 
-export default function Navbar() {
-  return (
-    <>
-    <nav className={styles.nav}>
-        <section className={styles.navLogo}>
-            <img src='./logo.svg' alt='Commscope' />
-        </section>
-        <section className={styles.navLinks}>
-            <ul>
-                <li>Home</li>
-                <li>ABOUT THE EVENT</li>
-                <li>AGENDA</li>
-                <li>VENUE</li>
-                <li>CONTACT</li>
-            </ul>
-        </section>
-        <section className={styles.navRegister}>
-            <button className={styles.registerButton}>
 
-            </button>
-        </section>
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <section className={styles.navLogo}>
+        <img src='./logo.svg' alt='Commscope' />
+      </section>
+      
+      <section className={`${styles.navLinks} ${isMenuOpen ? styles.active : ''}`}>
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT THE EVENT</li>
+          <li>AGENDA</li>
+          <li>VENUE</li>
+          <li>CONTACT</li>
+        </ul>
+      </section>
+      
+      <section className={styles.navRegister}>
+        <button className={styles.registerButton}></button>
+      </section>
+      
+      {/* Mobile menu toggle */}
+      <div className={styles.menuToggle} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
-    </>
-  )
+  );
 }

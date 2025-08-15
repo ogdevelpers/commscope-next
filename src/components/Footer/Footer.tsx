29ac7192e57220b08cfabfcdc59f42d4dd2d4e92
Somebro <someBroYouKnow@users.noreply.github.com>
2025-08-15 21:26:24 +0530

    footer done, need image of logo though

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,8 @@ export default function Footer(){
     return (
         <>
             <footer className={styles.footer}>
-                
+                <img src='./logo_footer.svg' alt='logo' />
+                <span className={styles.footerUrl}>www.commscope.com</span>
             </footer>
         </>
     )
